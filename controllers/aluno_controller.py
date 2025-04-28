940592cb1c56cbff965a6e2be67284abdc2a8b75
Antonioif <165713598+Antonioif@users.noreply.github.com>
2025-04-28 14:43:10 -0300

    novas rotas

--- controllers/aluno_controller.py
+++ controllers/aluno_controller.py
@@ -4,3 +4,15 @@ from flask import render_template
 @app.route("/aluno", methods=['GET'])
 def aluno():
     return render_template("/aluno/painel_aluno.html")
+
+@app.route("/aluno/dashboard", methods=['GET'])
+def aluno_dashboard():
+    return render_template("/usuario_aluno/dashboard.html")
+
+@app.route("/aluno/analise", methods=['GET'])
+def aluno_analise():
+    return render_template("/usuario_aluno/analise.html")
+
+@app.route("/teste", methods=['GET'])
+def teste():
+    return render_template("/usuario_aluno/dashboard.html")
