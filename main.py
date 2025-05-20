6f156b7cb7b615c8d98f0fe2eb83e9c6cefca358
antonio-jose-fernandes <110204549+antonio-jose-fernandes@users.noreply.github.com>
2025-05-20 16:45:55 -0300

    add modals avaliacao e testador

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from controllers.aluno_controller import *
 from controllers.analise_controller import *
 from controllers.amostra_controller import *
 
+from models.testador_modal import *
+from models.avaliacao_modal import *
+
 # Cria todas as tabelas no banco de dados
 Base.metadata.create_all(bind=engine)
 
---
+++ models/testador_modal.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from models.conexao import Base, engine
+
+class Testador(Base):
+    __tablename__ = "testadores"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    faixa_etaria = Column(String(255), nullable=False)
+    genero = Column(String(255), nullable=False) 
+   
