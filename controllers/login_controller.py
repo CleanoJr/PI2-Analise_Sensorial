adf465f385515e335731ff6cfc2bc17a4c28cbde
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2025-06-09 14:10:51 -0300

    Update login_controller.py

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -1,4 +1,11 @@
 from flask import flash
+from main import app
+from flask import request, render_template, redirect, url_for, flash
+from models.usuario_model import *
+from sqlalchemy.orm import sessionmaker  # Importação da sessionmaker
+
+# Criando a sessão para interagir com o banco de dados
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
 
 @app.route('/login', methods=['POST'])
 def login():
