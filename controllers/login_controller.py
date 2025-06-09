daef2723a16511b81a46daefdb7cf69e527442bc
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2025-06-09 14:26:54 -0300

    Update login_controller.py

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -9,23 +9,14 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 @app.route('/login', methods=['POST'])
 def login():
+    db = SessionLocal() 
+
     email = request.form['email']
     senha = request.form['senha']
+
+    if email == 'clara123@gmail.com' and senha == '123':
+        return redirect('/painel_aluno.html')
+    else
+        return redirect('/login.html')
          
-    ANALISE_DB = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM usuarios WHERE email = %s", (email,))
-    user = cursor.fetchone()
-    db.close()
 
-    if not user:
-        flash("Usuário não encontrado.", "danger")
-        return redirect(url_for('login.html'))
-
-    if not check_password_hash(user['senha'], senha):
-        flash("Senha incorreta.", "danger")
-        return redirect(url_for('login.html'))
-
-    session['usuario_id'] = user['id']
-    session['email'] = user['email']
-    return redirect(url_for('painel_aluno'))
