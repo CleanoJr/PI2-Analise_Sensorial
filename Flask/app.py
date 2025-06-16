f36545976aaab22fe21680f293521c93c49e12ff
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2025-06-16 19:01:19 -0300

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,5 +1,10 @@
+from main import app
+from models.usuario_model import *
+from models.conexao import *
+from datetime import datetime  # Para converter a data corretamente
+from sqlalchemy.orm import sessionmaker  # Importação da sessionmaker
+import re
 from flask import Flask, render_template, request, flash, redirect
-import json
 import os
 
 
