9df8d9e3ab42dba2dab0f9f1e9ca89d32578073e
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2025-06-16 16:55:30 -0300

    Update usuario_controller.py

--- controllers/usuario_controller.py
+++ controllers/usuario_controller.py
@@ -9,7 +9,7 @@ import re
 # Criando a sessão para interagir com o banco de dados
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-@app.route("/login", methods=['GET'])
+@app.route("/", methods=['GET'])
 def login():
     return render_template("/login.html")
 
