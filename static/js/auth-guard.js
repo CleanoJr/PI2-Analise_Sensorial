c9b1b75430cd24a443b3fb873eb279efbab120b9
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2025-06-22 17:45:10 -0300

    Update auth-guard.js

--- static/js/auth-guard.js
+++ static/js/auth-guard.js
@@ -1,5 +1,5 @@
 firebase.auth().onAuthStateChanged(user => {
     if (!user) {
-        window.location.href = "../../painel_admin.html";
+        window.location.href = "../../home.html";
     }
 })
