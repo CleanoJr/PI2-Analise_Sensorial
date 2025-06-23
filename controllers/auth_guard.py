6e30b50a2a223e34fc899f1ed12ee25fa6aef254
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2025-06-23 03:38:23 -0300

    Update auth_guard.py

--- controllers/auth_guard.py
+++ controllers/auth_guard.py
@@ -3,7 +3,7 @@ from functools import wraps
 from sqlalchemy.orm import sessionmaker  # Importação da sessionmaker
 
 # Criando a sessão para interagir com o banco de dados
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False)
 app = Flask(__name__)
 app.secret_key = 'segredo'
 
