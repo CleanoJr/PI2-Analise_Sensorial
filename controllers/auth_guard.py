3e5775f43f6c2a8a05bd7b989b17d5532fec7a17
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2025-06-23 03:36:35 -0300

    Update auth_guard.py

--- controllers/auth_guard.py
+++ controllers/auth_guard.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from functools import wraps
+from sqlalchemy.orm import sessionmaker  # Importação da sessionmaker
 
+# Criando a sessão para interagir com o banco de dados
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 app = Flask(__name__)
 app.secret_key = 'segredo'
 
@@ -12,6 +15,7 @@ def login_obrigatorio(f):
         flash('Faça login primeiro.', 'danger')
         return redirect(url_for('login'))
     return wrap
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
