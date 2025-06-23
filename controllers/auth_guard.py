af81ff15323cfea7116a3c8404654116de60157d
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2025-06-23 03:20:27 -0300

    Atualização

--- controllers/auth_guard.py
+++ controllers/auth_guard.py
@@ -5,14 +5,6 @@ from sqlalchemy.orm import sessionmaker  # Importação da sessionmaker
 from functools import wraps
 from flask import session, redirect, url_for, flash
 
-def login_obrigatorio(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if 'usuario_logado' not in session:
-            flash('Você precisa estar logado para acessar esta página.', 'danger')
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decorated_function
 # Criando a sessão para interagir com o banco de dados
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
